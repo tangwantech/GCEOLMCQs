d1e0df2149d1ecb92ae12377cd33f851e7a6af8b
tangwantech <tangwantech@gmail.com>
2023-07-22 21:30:13 +0100

    remove initializing activity

--- app/src/main/java/com/example/gceolmcq/activities/GCESplashActivity.kt
+++ app/src/main/java/com/example/gceolmcq/activities/GCESplashActivity.kt
@@ -2,12 +2,11 @@ package com.example.gceolmcq.activities
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gceolmcq.R
 import kotlinx.coroutines.*
 
-class SplashActivity : AppCompatActivity() {
+class GCESplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,30 +17,13 @@ class SplashActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch{
             delay(3000L)
             withContext(Dispatchers.Main){
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                val intent = Intent(this@GCESplashActivity, MainActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 startActivity(intent)
-//                exitActivity()
+//
             }
         }
-    }
-
-
-    private fun displayNoConnectionDialog(){
-//        indeterminateProgress.visibility = View.GONE
-        val alertDialog = AlertDialog.Builder(this)
-        alertDialog.apply {
-            setTitle(resources.getString(R.string.error))
-            setMessage(resources.getString(R.string.initialise_error_message))
-            setPositiveButton(resources.getString(R.string.exit)) { _, _ ->
-                exitActivity()
-            }
-            setCancelable(false)
-        }.create().show()
-    }
-    private fun exitActivity(){
-        finish()
     }
 
 }
